bff3cadc8eca1747b7d1f47d52a15b8193a21af9
punkmohamed <punkmohamed22@gmail.com>
2024-11-02 11:25:45 +0200

    logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar = async () => {
         <div className="px-5 py-3 bg-white shadow-sm font-work-sans">
             <nav className="flex justify-between items-center">
                 <Link href='/'>
-                    <Image src='/logo.png' alt="logo" width={144} height={30} />
+                    <Image src='/logo.webp' alt="logo" width={144} height={30} />
                 </Link>
                 <div className="items-center gap-5 flex text-black">
                     {session && session?.user ? (
